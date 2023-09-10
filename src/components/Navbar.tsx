cbb6ad6acbef19d9e3a9bcf59914b12034fd6ee6
Gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-09-10 10:19:04 -0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import { BookMarked } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between">
+      <BookMarked />
+      <h2>Dictionary</h2>
+    </nav>
+  );
+}
